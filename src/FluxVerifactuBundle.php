20dab458ec78a6888ed9bcd7e306b8962a1fed43
David Romaní <david@flux.cat>
2025-12-17 18:46:58 +0100

    Add `ContainerBuilder` to `loadExtension` for extended DI configuration options in `FluxVerifactuBundle`.

--- src/FluxVerifactuBundle.php
+++ src/FluxVerifactuBundle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Flux\VerifactuBundle;
 
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
@@ -18,7 +19,7 @@ final class FluxVerifactuBundle extends AbstractBundle
         $definition->import('../config/definition.php');
     }
 
-    public function loadExtension(array $config, ContainerConfigurator $container): void
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
         $container->import('../config/services.php');
     }
