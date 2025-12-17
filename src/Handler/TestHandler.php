8d4e5028b67b0caabd0c71706ac664fda683fb16
David Romaní <david@flux.cat>
2025-12-17 19:54:35 +0100

    remove unnecessary Autowire from TestHandler

--- src/Handler/TestHandler.php
+++ src/Handler/TestHandler.php
@@ -4,18 +4,15 @@ declare(strict_types=1);
 
 namespace Flux\VerifactuBundle\Handler;
 
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
-
 final class TestHandler
 {
     public function __construct(
-        #[Autowire(param: '%kernel.default_locale%')]
         private readonly string $isProdEnvironment
     ) {
     }
 
     public function getTest(): string
     {
-        return $this->isProdEnvironment;
+        return $this->isProdEnvironment ? 'true' : 'false';
     }
 }
