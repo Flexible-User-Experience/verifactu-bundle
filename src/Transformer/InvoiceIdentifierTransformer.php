e45cee7241a90fff228a9cfd354f19cf21c6c1aa
David Romaní <david@flux.cat>
2025-12-26 10:55:48 +0100

    add InvoiceIdentifierTransformer

---
+++ src/Transformer/InvoiceIdentifierTransformer.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flux\VerifactuBundle\Transformer;
+
+use Flux\VerifactuBundle\Contract\InvoiceIdentifierInterface;
+use Flux\VerifactuBundle\Dto\InvoiceIdentifierDto;
+use josemmo\Verifactu\Models\Records\InvoiceIdentifier;
+
+final readonly class InvoiceIdentifierTransformer extends BaseTransformer
+{
+    public function transformInterfaceToModel(InvoiceIdentifierInterface $input): InvoiceIdentifierDto
+    {
+        return new InvoiceIdentifierDto(
+            issuerId: BaseTransformer::tt($input->getIssuerId(), 9),
+            invoiceNumber: BaseTransformer::tt($input->getInvoiceNumber(), 60),
+            issueDate: $input->getIssueDate(),
+        );
+    }
+
+    public function transformDtoToModel(InvoiceIdentifierDto $dto): InvoiceIdentifier
+    {
+        $invoiceIdentifier = new InvoiceIdentifier();
+        $invoiceIdentifier->issuerId = $dto->getIssuerId();
+        $invoiceIdentifier->invoiceNumber = $dto->getInvoiceNumber();
+        $invoiceIdentifier->issueDate = $dto->getIssueDate();
+
+        return $invoiceIdentifier;
+    }
+}
