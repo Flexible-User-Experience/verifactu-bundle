bfd4bfb18e5e57efc709f463d2150bf61f7d6a3c
David Romaní <david@flux.cat>
2025-12-27 16:58:46 +0100

    add Contract/AeatResponseInterface

---
+++ src/Contract/AeatResponseInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flux\VerifactuBundle\Contract;
+
+use josemmo\Verifactu\Models\Responses\ResponseStatus;
+
+interface AeatResponseInterface extends ValidatableInterface
+{
+    public function getCsv(): ?string;
+
+    public function getSubmittedAt(): ?\DateTimeInterface;
+
+    public function getWaitSeconds(): int;
+
+    public function getStatus(): ResponseStatus;
+
+    public function getItems(): array;
+}
